0b0fa620c0d59cb7ba4286715ae00e32b69e2566
mikhail.volkov <mvvolkov@mail.ru>
2022-04-28 13:50:49 +0300

    MoveZeroesTest improvements

--- src/leetCode/easy/MoveZeroesTest.java
+++ src/leetCode/easy/MoveZeroesTest.java
@@ -5,22 +5,35 @@ import org.junit.Test;
 import java.util.Arrays;
 
 /**
- * Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ * <a href="https://leetcode.com/problems/move-zeroes/">https://leetcode.com/problems/move-zeroes/</a>
+ * <p>
+ * 283. Move Zeroes
+ * <p>
+ * Given an integer array nums, move all 0's to the end of it while maintaining the relative order of
+ * the non-zero elements.
  * <p>
  * Note that you must do this in-place without making a copy of the array.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [0,1,0,3,12]
+ * Output: [1,3,12,0,0]
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: nums = [0]
+ * Output: [0]
+ * <p>
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= nums.length <= 104
+ * -231 <= nums[i] <= 231 - 1
  */
 public class MoveZeroesTest {
-
-
-    //Example 1:
-    //
-    //Input: nums = [0,1,0,3,12]
-    //Output: [1,3,12,0,0]
-    //
-    //Example 2:
-    //
-    //Input: nums = [0]
-    //Output: [0]
 
     @Test
     public void test1() {
@@ -51,21 +64,20 @@ public class MoveZeroesTest {
         assert Arrays.equals(nums, expected);
     }
 
-
     public static void moveZeroes(int[] nums) {
 
-        int k = 0;
         int size = nums.length;
-        boolean zeroFound = false;
-        for (int i = 0; i < size; i++) {
+        int firstZero = 0;
+        for (; firstZero < size; firstZero++) {
+            if (nums[firstZero] == 0) {
+                break;
+            }
+        }
+        int k = firstZero;
+        for (int i = firstZero + 1; i < size; i++) {
             int value = nums[i];
             if (value != 0) {
-                if (zeroFound) {
-                    nums[k] = value;
-                }
-                k++;
-            } else {
-                zeroFound = true;
+                nums[k++] = value;
             }
         }
         while (k < size) {
